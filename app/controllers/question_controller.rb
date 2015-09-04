66f01be6bcbbe350d73b3a85ea82166a2767d3f1
Jonathan <jlvedder@aol.com>
2015-09-04 07:49:54 -0700

    first commit

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,23 @@
+class QuestionController < ApplicationController
+  def index
+    @question= Question.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
