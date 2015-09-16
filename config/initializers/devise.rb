32e279cfbeb551bc7b92b4a6ee176220e7f507f6
Jonathan <jlvedder@aol.com>
2015-09-16 08:20:49 -0700

    configured apps for emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'confirm login'
+  config.mailer_sender = 'jlvedder@aol.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
