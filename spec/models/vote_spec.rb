5d4e3f2e0d574c7a83156e4862c87056d146b6f2
Jonathan <jlvedder@aol.com>
2015-10-07 08:12:31 -0700

    added vote test

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        vote1 = Vote.new(value: 1)
+        vote2 = Vote.new(value: -1)
+        vote3 = Vote.new(value: 2)
+        expect(vote1.valid?).to eq(true)
+        expect(vote2.valid?).to eq(true)
+        expect(vote3.valid?).to eq(false)
+      end
+    end
+  end
+end
